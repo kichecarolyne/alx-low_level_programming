865281fbc6391db2259a9e3bab0c812a9b68828b
Kiche Carolyne <kichec@gmail.com>
2022-12-08 18:20:44 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-int main void()
-{
-puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+
+int main(void) {
+
+	/*puts is used to print a message*/
+	puts("Programming is like building a multilingual puzzle");
+
+	return 0;
 }
