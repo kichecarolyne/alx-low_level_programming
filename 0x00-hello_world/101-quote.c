eecfd9edad794d85b551bd5368ed69054286d8de
Kiche Carolyne <kichec@gmail.com>
2022-12-09 14:17:21 -0800

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'prints a string to the standard error'
+ *
+ * Return: Always 1 (Error)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	getchar();
+	return (1);
+}
