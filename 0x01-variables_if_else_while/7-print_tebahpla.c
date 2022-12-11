59ec32766433a75e8986e83d3e844294ed4fae0a
Kiche Carolyne <kichec@gmail.com>
2022-12-11 01:02:27 -0800

    Q

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: ''
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z')
+		{
+		reverseStr(str);	
+		putchar(c);
+		++c;
+		}
+	putchar('\n');
+	return (0);
+}
