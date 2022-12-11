74e1acca0d7882575c508e8f9578f0854492964f
Kiche Carolyne <kichec@gmail.com>
2022-12-10 21:12:32 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints the alphabet in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'b';
+
+	for (ch = 'b'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
+
