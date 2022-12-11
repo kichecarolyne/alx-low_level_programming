3453af281c27ac54a14419e3192104c2a7f97d79
Kiche Carolyne <kichec@gmail.com>
2022-12-10 23:31:55 -0800

    Except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: ''
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z')
+		{
+		if ((c != 'q' && c != 'e') && c <= 'z')
+		putchar(c);
+		++c;
+		}
+	putchar('\n');
+	return (0);
+}
