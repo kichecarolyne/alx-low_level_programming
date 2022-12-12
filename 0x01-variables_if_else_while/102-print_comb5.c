9a2e41007ba4848fe008368b597dc8d69cbbeb28
Kiche Carolyne <kichec@gmail.com>
2022-12-12 02:09:16 -0800

    Combo5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,7 +23,7 @@ int main(void)
 		putchar((j / 10) + '0');
 		putchar((j % 10) + '0');
 
-		if (i != 98 || j != 99)
+		if (i != 0 || i != 98 || j != 0 || j != 99)
 		putchar(',');
 		putchar(' ');
 		}
