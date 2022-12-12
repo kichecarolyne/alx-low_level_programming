f22254e75c2d771df48514b151636a8e5f4890fa
Kiche Carolyne <kichec@gmail.com>
2022-12-12 02:53:01 -0800

    Combo5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,18 +12,16 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 100; i++)
-	for (j = 0; j < 100; j++)
+	for (i = 0; i < 98; i++)
+	for (j = 0; j < 99; j++)
 		{
-		if (i < j)
-
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
 		putchar(' ');
 		putchar((j / 10) + '0');
 		putchar((j % 10) + '0');
 
-		if (i != 0 || i != 98 || j != 0 || j != 99)
+		if (i == 98 && j == 99)
 		putchar(',');
 		putchar(' ');
 		}
