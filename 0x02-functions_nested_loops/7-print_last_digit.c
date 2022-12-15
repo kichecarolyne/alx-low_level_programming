20ee8f03ddcab6debda93ea6d0f16f8e826eda95
Kiche Carolyne <kichec@gmail.com>
2022-12-14 17:44:26 -0800

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,12 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int n = x % 10;
 
-	if (last_digit < 0)
-		last_digit *= -1;
+	if (n < 0)
+		n *= -1;
 
-	_putchar(last_digit + '0');
+	_putchar(n + '0');
 
-	return (last digit);
+	return (n);
 }
