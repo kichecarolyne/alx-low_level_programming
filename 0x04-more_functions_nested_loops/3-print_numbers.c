3a0bc58a9a3a669145bcf35de144bb668a5c63a5
Kiche Carolyne <kichec@gmail.com>
2022-12-19 22:04:49 -0800

    Print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,15 @@
  * print_numbers - prints 0 to 9
  * Return: void
  */
+
 void print_numbers(void)
+
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-_putchar(c);
-}
-_putchar('\n')
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n')
 }	
