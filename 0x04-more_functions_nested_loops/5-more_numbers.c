79436ab0beab8dcd3385bb135a11a68814a32a5a
Kiche Carolyne <kichec@gmail.com>
2022-12-19 23:41:13 -0800

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,11 @@ void more_numbers(void)
 {
 	int c, d;
 
-	for (c = 1; c <= 10; c++)
+	for (c = 0; c <= 10; c++)
 	{
-	for (d = 0; d <= 14; d++)
+	for (d = 0; d <= 15; d++)
 	{
-	if (d >= 10)
-	_putchar('1');
-	_putchar(d % 10 + '0');
-	}
+	_putchar (y + '0');
 	}
 	_putchar('\n');
 }
