6482e87048a85e95a454dbe0a6a6e4543af102c0
Kiche Carolyne <kichec@gmail.com>
2022-12-22 04:06:31 -0800

    Capital

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,16 +12,16 @@ char *cap_string(char *s)
 	int len = 13;
 	int a = 0, i;
 
-	while (s[a])
+	while (x[a])
 	{
 	i = 0;
 	while (i < len)
 	{
-	if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-	s[a] = s[a] - 32;
+	if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+	x[a] = x[a] - 32;
 	1++;
 	}
 	a++;
 	}
-	return (s);
+	return (x);
 }
