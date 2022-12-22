61547072191149a0439064dafe89d2ca3b9ed288
Kiche Carolyne <kichec@gmail.com>
2022-12-22 07:44:03 -0800

    Capital

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,31 @@
 #include "main.h"
 /**
  * cap_string - capitalizes all words in a string
- * @x: string
+ * @s: string
  * Return: capital version of the string
  */
 
 char *cap_string(char *s)
 
 {
-	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-	int len = 13;
-	int a = 0, i;
-
-	while (x[a])
+	int c = 0, d;
+	char a[] = "'\t','\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'";
+	while (*(s + c))
 	{
-	i = 0;
-	while (i < len)
+	if (*(s + c) >= 'a' && *(s + c) <= 'z')
 	{
-	if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
-	x[a] = x[a] - 32;
-	1++;
+	if (c == 0)
+	*(s + c) -= 'a' - 'A';
+	else
+	{
+	for (d = 0; d <= 12; d++)
+	{
+	if (a[d] == *(s + c - 1))
+	*(s + c) -= 'a' - 'A';
 	}
-	a++;
 	}
-	return (x);
+	}
+	c++;
+	}
+	return (s);
 }
