d6f3749840d1f4925de7eb679515a9332124d24a
Kiche Carolyne <kichec@gmail.com>
2022-12-22 03:31:27 -0800

    Capital

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ */
+
+char *cap_string(char *s)
+
+{
+	int c = 0, d;
+	char a[] = "\t\t,;.!?\"(){}";
+
+	while (*(s + c))
+	{
+	if (*(s + c) >= 'a' && *(s + c) <= 'z')
+	{
+	if (c == 0)
+	*(s + c) -= 'a' - 'A';
+	else
+	{
+	for (d = 0; d <= 12; d++)
+	{
+	if (a[d] == *(s + c -1))
+	*(s + c) -= 'a' - 'A';
+	}
+	}
+	}
+	c++;
+	}
+	return (s);
+}
