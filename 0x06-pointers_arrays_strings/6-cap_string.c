4050bb719404b47fe2b6baa5f775e511135eb336
Kiche Carolyne <kichec@gmail.com>
2022-12-22 13:10:08 -0800

    hot fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,27 +6,29 @@
  */
 
 char *cap_string(char *s)
-
 {
-	int c = 0, d;
-	char a[] = '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}';
+	int count;
 
-	while (*(s + c))
+	/*  scan through string */
+	count = 0;
+	while (s[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+	if (s[0] >= 97 && s[0] <= 122)
 	{
-	if (*(s + c) >= 'a' && *(s + c) <= 'z')
+	s[0] = s[0] - 32;
+	}
+	if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+	|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+	|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+	|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+	|| s[count] == '{' || s[count] == '}')
 	{
-	if (c == 0)
-	*(s + c) -= 'a' - 'A';
-	else
+	if (s[count + 1] >= 97 && s[count + 1] <= 122)
 	{
-	for (d = 0; d <= 12; d++)
-	{
-	if (a[d] == *(s + c - 1))
-	*(s + c) -= 'a' - 'A';
+	s[count + 1] = s[count + 1] - 32;
 	}
 	}
-	}
-	c++;
+	count++;
 	}
 	return (s);
 }
