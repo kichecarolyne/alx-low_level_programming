2024b6c29773b7c070a86bd1ae22d2e787a90fea
Kiche Carolyne <kichec@gmail.com>
2022-12-23 08:12:27 -0800

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ * @n: param
+ */
+
+void print_line(int n)
+
+{
+	int c = 1;
+
+	while (c <= n)
+	{
+	_putchar(95);
+	c++;
+	}
+	_putchar('\n');
+}
