3b9321a97eb53ddbae6df31c719316f3a73330c1
Kiche Carolyne <kichec@gmail.com>
2022-12-23 12:00:56 -0800

    Fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,11 @@ int main(void)
 	for (c = 1; c <= 100; c++)
 	{
 	if ((c % 3 == 0) && (c % 5 == 0))
-	printf("FizzBuzz ");
+	printf("FizzBuzz");
 	else if (c % 3 == 0)
-	printf("Fizz ");
+	printf("Fizz");
 	else if (c % 5 == 0)
-	printf("Buzz ");
+	printf("Buzz");
 	else
 	printf("%d", c);
 	}
