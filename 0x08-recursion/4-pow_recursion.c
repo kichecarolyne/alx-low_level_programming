df188135e3e16d4b5142ef545af18e1e4637163b
Kiche Carolyne <kichec@gmail.com>
2023-01-05 09:07:19 -0800

    hotfix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,10 +9,14 @@
 int _pow_recursion(int x, int y)
 
 {
-	int result = x;
-
 	if (y < 0)
+	{
 	return (-1);
-	result *= _pow_recursion(x, y - 1);
-	return (result);
+	}
+	else if (y != 0)
+	return (x * _pow_recursion(x, y - 1));
+	else
+	{
+	return (1);
+	}
 }
