bb893d9a2b9080c89a77b11b560971dabae029fd
Kiche Carolyne <kichec@gmail.com>
2023-01-09 13:15:21 -0800

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the name of the program
+ * @argc: counts arguments
+ * @argv: arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int count = 0;
+
+	if (argc > 0)
+	while (count < argc)
+	{
+	printf("%s\n", argv[count]);
+	count++;
+	}
+	return (0);
+}
