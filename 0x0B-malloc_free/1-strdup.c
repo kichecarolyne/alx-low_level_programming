eaeaa2c03ba66acc65a325ecae8473ad219dd922
Kiche Carolyne <kichec@gmail.com>
2023-01-11 18:13:54 -0800

    hotfix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,20 +9,18 @@
 char *_strdup(char *str)
 
 {
-	int i = 0, j = 0;
+	int i, j;
 	char *s;
 
 	if (str == NULL)
 	return (NULL);
-	for (str[i])
-		i++;
+	for (i = 0; str[i]; i++)
 	s = malloc(i * sizeof(char));
 	if (s == NULL)
 	{
 	return (NULL);
 	}
-	for (j < i)
-		j++;
+	for (j = 0; j < i; j++)
 	{
 	s[j] = str[j];
 	}
